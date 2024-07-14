c4dad691b115c0753c049839e3ce33661cf158ac
Jason Wong <jasonwong_3@hotmail.com>
2024-07-14 18:50:26 +0100

    feat: bio animation WIP 2

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ function setScrollVar() {
 
 setScrollVar()
 
+
+const about2 = document.getElementById("about-text-2");
+const about3 = document.getElementById("about-text-3");
+const about4 = document.getElementById("about-text-4");
+
+// const screenHeight = htmlElement.clientHeight;
+window.addEventListener('scroll', () => {
+  if (window.scrollY < 1300) {
+    about2.style.opacity = "0"
+    about3.style.opacity = "0"
+    about4.style.opacity = "0"
+  }
+  if (window.scrollY >= 1300 && window.scrollY < 2300) {
+    about2.style.opacity = "1"
+    about3.style.opacity = "0"
+    about4.style.opacity = "0"
+  }
+  if (window.scrollY >= 2300 && window.scrollY < 3300) {
+    about2.style.opacity = "0"
+    about3.style.opacity = "1"
+    about4.style.opacity = "0"
+  } 
+  if (window.scrollY >= 3300 && window.scrollY < 4300) {
+    about2.style.opacity = "0"
+    about3.style.opacity = "0"
+    about4.style.opacity = "1"
+  } 
+})

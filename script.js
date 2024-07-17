a5128298f1f708b61f54a9c703d297775f82bbc7
Jason Wong <jasonwong_3@hotmail.com>
2024-07-17 16:36:05 +0100

    feat: custom cursor WIP 1

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const cursor = document.querySelectorAll(".cursor");
+const links = document.querySelectorAll("#title-main span, a, button");
+
+window.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+  cursor.forEach(el => {
+    el.style.left = `${x}px`;
+    el.style.top = `${y}px`;
+    
+    links.forEach(link => {
+      link.addEventListener("mouseenter", () => {
+          el.classList.add("hover");
+        })
+        link.addEventListener("mouseleave", () => {
+            el.classList.remove("hover");
+      })
+    })
+  })
+})
+
+
 // window.addEventListener("scroll", setScrollVar)
 // window.addEventListener("resize", setScrollVar)
 
